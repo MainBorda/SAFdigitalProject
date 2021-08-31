45a01a8907fb0b0fbab601ccf954e43898f7da76
ByDrox37 <89815940+ByDrox37@users.noreply.github.com>
2021-08-31 15:09:46 -0500

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,48 @@
+<?php
+include("plantillas/header.php");
+?>
+<?php
+include("plantillas/dashboard.php");
+?>
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+      <symbol id="check" viewBox="0 0 16 16">
+        <title>Check</title>
+        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
+      </symbol>
+    </svg>
+    <div class="container py-3">
+      <header>
+        <div class="pricing-header p-3 pb-md-4 mx-auto text-center" >
+          <h1 class="display-4 fw-normal" >NOSOTROS</h1>
+          </div>
+      </header>
+  <img src= "imagenes/logo.jpg" width="500" height="300" align="right">
+  
+<main>
+    <div class="container-fluid">
+        <h2>HISTORIA DE LA EMPRESA</h2>
+    </div>
+
+</main>
+<br>
+<div class="container-fluid" >
+        <h3>Somos una nueva empresa que estamos construyendo nuestro
+            exito en este rubro el cual se dedica al marketing digital, 
+            para asi poder convertirnos en la mejor opción de las empresas
+            que nos requieran. Además de hacer que las micro pequeñas y medianas
+            empresas hagan crecer su negocio.
+          <br>
+            Nuestro exito se va a basar en las mejores tendencias de marketing digital,
+            con los cuales le podremos ofrecer a nuestros clientes soluciones innovadoras 
+            y eficaces a un costo moderado.
+          </h3>
+        
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+
+<?php
+include("plantillas/footer.php");
+?>
